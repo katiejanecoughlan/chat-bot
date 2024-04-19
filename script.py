9749161fe28f6b7c2078aed3694de767c4663b26
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-04-19 15:20:52 +0000

    Create code to receive and repeat user inputs

---
+++ script.py
@@ -0,0 +1,10 @@
+print("Welcome to RepeatBot!")
+
+print("""
+So, what is RepeatBot?
+It's a chatbot that will repeat what you say!
+""")
+
+stuff_to_echo = input("Say something:")
+
+print("You said: " + stuff_to_echo)
