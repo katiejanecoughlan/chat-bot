1c5f34d8db735dde8843cf67646f1e9f969d5caa
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-04-19 15:23:12 +0000

    Add try again option

--- script.py
+++ script.py
@@ -8,3 +8,9 @@ It's a chatbot that will repeat what you say!
 stuff_to_echo = input("Say something:")
 
 print("You said: " + stuff_to_echo)
+
+print("Want to try again?")
+
+stuff_to_echo = input("Say something else:")
+
+print("You said: " + stuff_to_echo)
