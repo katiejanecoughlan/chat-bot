828499871a6ba8aa6a4a0697a10dfa1109867668
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-04-19 15:35:51 +0000

    Create i went to the shop and i bought code

---
+++ shop.py
@@ -0,0 +1,20 @@
+print("""
+Welcome to your favourite childhood game:
+ I Went to the Shop and I bought!
+ """)
+
+stuff_to_buy = input("What did you buy?")
+
+print("I went to the shop and I bought " + stuff_to_buy)
+
+print("Then...")
+
+stuff_to_buy_2 = input("What did you buy?")
+
+print("I went to the shop and I bought: " + stuff_to_buy + " and " + stuff_to_buy_2)
+
+print("Then...")
+
+stuff_to_buy_3 = input("What did you buy?")
+
+print("I went to the shop and I bought: " + stuff_to_buy + ", " + stuff_to_buy_2 + " and " + stuff_to_buy_3)
